ba2c6ab8cff0cbdc4f49f019f825f59cb128f46a
Sai Varadula <sai.varadula@gmail.com>
2022-05-15 16:37:22 +0530

    changing port to 3001

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
 
 app.use('/', routes);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}.`);
 });
