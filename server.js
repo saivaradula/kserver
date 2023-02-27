2aa1ac99402343b9d5ca27c3b142ecf858adc938
svaradula <svaradula@launchcg.com>
2023-02-27 14:39:00 +0530

    IP changed

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const routes = require('./app/routes');
 
 var corsOptions = {
 	// origin: "http://localhost:3002"
-	origin: 'http://ec2-3-234-250-196.compute-1.amazonaws.com',
+	origin: 'http://44.200.120.203',
 };
 
 app.use(cors(corsOptions));
